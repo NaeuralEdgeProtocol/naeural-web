34ba7495293a34836e193cc5158d26b185fca74d
Andrei Damian <andrei.damian@naeural.ai>
2024-07-08 16:14:35 +0300

    fix: sidebar

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const navLinks = document.querySelectorAll('nav a');
+  const sidebarToggle = document.querySelector('.sidebar-toggle');
+  const sidebar = document.querySelector('.sidebar');
+  const content = document.querySelector('.content');
 
-  const highlightNav = () => {
-    let index = sections.length;
-
-    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
-
-    navLinks.forEach((link) => link.classList.remove('active'));
-    navLinks[index].classList.add('active');
-  };
-
-  const sections = document.querySelectorAll('section');
-  window.addEventListener('scroll', highlightNav);
+  sidebarToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+    content.classList.toggle('collapsed');
+  });
 });
