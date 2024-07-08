28b80f2e6b0c8453700f510740e62123c504c480
Andrei Damian <andrei.damian@naeural.ai>
2024-07-08 18:10:09 +0300

    fix: logo

--- public/scripts.js
+++ public/scripts.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const sidebarToggle = document.querySelector('.sidebar-toggle');
   const sidebar = document.querySelector('.sidebar');
   const content = document.querySelector('.content');
+  const footer = document.querySelector('footer');
 
   sidebarToggle.addEventListener('click', () => {
     sidebar.classList.toggle('collapsed');
     content.classList.toggle('collapsed');
+    footer.classList.toggle('collapsed');
   });
 });
